89658c2545ce5d4195ea805b5346a155c1005cc6
mickamy <tetsuro@mickamy.com>
2024-12-31 13:01:39 +0900

    fix: tailwind style

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import tailwindStyles from "./tailwind.css";
+import tailwindStyles from "~/tailwind.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: tailwindStyles },
