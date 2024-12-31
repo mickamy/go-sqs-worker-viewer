5a7a2f8cbec80cca6d85ba6af5265be7b0b4b62d
mickamy <tetsuro@mickamy.com>
2024-12-31 21:22:32 +0900

    style: make container to fill screen height

--- app/components/container.tsx
+++ app/components/container.tsx
@@ -8,7 +8,7 @@ export default function Container({ children, className, ...props }: Props) {
   return (
     <div
       className={cn(
-        "container mx-auto -h-full flex w-full flex-col justify-start px-12 py-8 lg:p-8",
+        "container mx-auto h-screen flex w-full flex-col justify-start px-12 py-8 lg:p-8",
         className
       )}
       {...props}
