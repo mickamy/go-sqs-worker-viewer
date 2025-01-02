c6d2ee8326a30e031bedbbb6ceb75b42c806a17a
mickamy <tetsuro@mickamy.com>
2025-01-02 18:39:23 +0900

    fix: items not loaded

--- app/routes/api.jobs/route.ts
+++ app/routes/api.jobs/route.ts
@@ -1,5 +1,6 @@
 import { LoaderFunction } from "@remix-run/node";
 
+import { Job } from "~/models/job";
 import { JobStatus, JobStatuses } from "~/models/job-statistics";
 import { getJobs } from "~/service/job-service";
 
@@ -9,15 +10,21 @@ export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const params = url.searchParams;
   const status = params.get("status");
-  const cursor = parseInt(params.get("cursor") || "0", 10);
+  let cursor = parseInt(params.get("cursor") || "0", 10);
   if (!status || !(JobStatuses as string[]).includes(status)) {
     throw new Response(null, { status: 404, statusText: "Not Found" });
   }
 
-  const { jobs, nextCursor } = await getJobs({
-    status: status as JobStatus,
-    cursor,
-    chunkSize,
-  });
-  return Response.json({ jobs, nextCursor });
+  const jobs: Job[] = [];
+  do {
+    const { jobs: pageJobs, nextCursor } = await getJobs({
+      status: status as JobStatus,
+      cursor,
+      chunkSize,
+    });
+    jobs.push(...pageJobs);
+    cursor = nextCursor;
+  } while (cursor > 0 && jobs.length == 0);
+
+  return Response.json({ jobs, nextCursor: cursor });
 };
