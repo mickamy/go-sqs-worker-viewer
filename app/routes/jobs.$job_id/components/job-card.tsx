e186010fbb1004d6302e26559eaf7c5e96a1db09
mickamy <tetsuro@mickamy.com>
2025-01-03 12:14:30 +0900

    feat: make job status to be changeable

--- app/routes/jobs.$job_id/components/job-card.tsx
+++ app/routes/jobs.$job_id/components/job-card.tsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 
 import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 import {
   Dialog,
   DialogContent,
@@ -11,6 +17,14 @@ import {
   DialogTrigger,
 } from "~/components/ui/dialog";
 import { ScrollArea } from "~/components/ui/scroll-area";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import { cn } from "~/lib/utils";
 import { Job } from "~/models/job";
 import { JobStatus } from "~/models/job-statistics";
 
@@ -27,9 +41,25 @@ interface Props {
 
 export default function JobCard({ job, onStatusChange }: Props) {
   const [isPayloadOpen, setIsPayloadOpen] = useState(false);
+  const [isStatusDialogOpen, setIsStatusDialogOpen] = useState(false);
+  const [newStatus, setNewStatus] = useState<JobStatus | null>(null);
+
+  const handleStatusChange = (status: JobStatus) => {
+    setNewStatus(status);
+  };
+
+  const handleStatusSubmit = () => {
+    if (newStatus) {
+      onStatusChange({ id: job.id, newStatus });
+      setIsStatusDialogOpen(false);
+      setNewStatus(null);
+    }
+  };
+
+  const isStatusChangeable = selectableStatuses(job.status).length > 0;
 
   return (
-    <Card className="w-full max-w-3xl mx-auto min-w-[400px]">
+    <Card className="w-full max-w-3xl mx-auto min-w-[400px] relative">
       <CardHeader>
         <CardTitle className="text-2xl">
           <div className="flex flex-row items-center space-x-4">
@@ -75,7 +105,7 @@ export default function JobCard({ job, onStatusChange }: Props) {
             <dd className="mt-2">
               <Dialog open={isPayloadOpen} onOpenChange={setIsPayloadOpen}>
                 <DialogTrigger asChild>
-                  <Button variant="outline" size="lg">
+                  <Button variant="outline" size="sm">
                     View Payload
                   </Button>
                 </DialogTrigger>
@@ -95,15 +125,64 @@ export default function JobCard({ job, onStatusChange }: Props) {
               <dt className="text-base text-gray-500">Created At</dt>
               <dd className="mt-2 text-base">{formatDate(job.created_at)}</dd>
             </div>
-            <div className="col-span-1">
+            <div className="col-span-1 flex flex-col">
               <dt className="text-base text-gray-500">Updated At</dt>
               <dd className="mt-2 text-base">{formatDate(job.updated_at)}</dd>
             </div>
           </div>
         </dl>
       </CardContent>
+      <CardFooter
+        className={cn("flex justify-end", !isStatusChangeable && "hidden")}
+      >
+        <Button variant="default" onClick={() => setIsStatusDialogOpen(true)}>
+          Change Job Status
+        </Button>
+      </CardFooter>
+      <Dialog open={isStatusDialogOpen} onOpenChange={setIsStatusDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Change Job Status</DialogTitle>
+          </DialogHeader>
+          <Select
+            onValueChange={handleStatusChange}
+            value={newStatus || undefined}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Select new status" />
+            </SelectTrigger>
+            <SelectContent>
+              {selectableStatuses(job.status).map((status) => (
+                <SelectItem key={status} value={status}>
+                  {formatStatus(status)}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <Button onClick={handleStatusSubmit} disabled={!newStatus}>
+            Submit
+          </Button>
+        </DialogContent>
+      </Dialog>
     </Card>
   );
+}
+
+function selectableStatuses(status: JobStatus): JobStatus[] {
+  switch (status) {
+    case "queued":
+      return ["success", "failed"];
+    case "processing":
+      return [];
+    case "retrying":
+      return ["success", "failed"];
+    case "success":
+      return [];
+    case "failed":
+      return [];
+    default:
+      return [];
+  }
 }
 
 function formatDate(date: string): string {
