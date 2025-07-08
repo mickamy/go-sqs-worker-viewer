c58ce3604e08065d8f143d834d6d2f25ae41393e
Tetsuro Mikami <tetsuro@mickamy.com>
2025-07-08 11:19:44 +0900

    fix: do not use aws credentials if not specified

--- app/lib/aws/sqs/index.ts
+++ app/lib/aws/sqs/index.ts
@@ -1,21 +1,21 @@
 import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
 
 const {
-  AWS_REGION = "ap-northeast-1",
-  AWS_ACCESS_KEY_ID = "",
-  AWS_SECRET_ACCESS_KEY = "",
+  AWS_REGION,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
   SQS_ENDPOINT_URL,
 } = process.env;
 
 const client = new SQSClient({
-  region: AWS_REGION,
-  credentials: {
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-  },
-  ...(SQS_ENDPOINT_URL
-    ? { endpoint: SQS_ENDPOINT_URL, useQueueUrlAsEndpoint: true }
-    : {}),
+  region: AWS_REGION!,
+  credentials: process.env.AWS_ACCESS_KEY_ID
+    ? {
+        accessKeyId: AWS_ACCESS_KEY_ID!,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY!,
+      }
+    : undefined,
+  endpoint: SQS_ENDPOINT_URL || undefined,
 });
 
 export async function enqueueToSqs({
